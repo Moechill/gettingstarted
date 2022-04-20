48baaa2e2a0221ae8693b627128ee93b74e5d100
Moechill <77429917+Moechill@users.noreply.github.com>
2022-04-20 11:04:40 -0500

    server überarteitet else if, server runs until shoutdown with ctrl c

--- server.js
+++ server.js
@@ -5,16 +5,12 @@ const myserver = http.createServer((req, res) =>{
         res.end('welcome to my new server')
     }
 
-    if (req.url === '/about'){
+    else if (req.url === '/about'){
         res.end('the about you page')
     }
     
-    res.end(`
-    <h1> OOOOOpppaa </h1>
-<p> Something went wrong, click here to go back to main -> </p>
-<a href = "/"> back home </a>
-    `)
-    
+    else{res.end('page not found')
+    }
 })
 
 myserver.listen(5000)
