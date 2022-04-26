8a9ea726f2f866b31cfa565b4af933fcbb68696a
Moechill <77429917+Moechill@users.noreply.github.com>
2022-04-26 10:22:11 -0500

    test of nodemon

--- second.js
+++ second.js
@@ -1,1 +1,8 @@
 console.log ('nodemon is really cool, it allows me to have a faster workflow through directly run my code after saving')
+
+const x = 'nodemon'
+
+if (x == 'nodemon'){
+    console.log('very fast way to run code')
+    
+}
